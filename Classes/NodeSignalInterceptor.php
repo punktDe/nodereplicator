797b8bd195d20516b83f5f36d19dd54e6cbf87b7
hwala <henry.wala@paessler.com>
2023-05-22 13:18:45 +0200

    feat: add option to only replicate nodes with certain starting paths

--- Classes/NodeSignalInterceptor.php
+++ Classes/NodeSignalInterceptor.php
@@ -19,8 +19,8 @@ class NodeSignalInterceptor
      */
     public static function nodeAdded(NodeInterface $node): void
     {
-        if (self::hasReplicationConfiguration($node) && (self::nodeCreateReplicationEnabled($node) || self::nodeCreateHiddenEnabled($node))) {
-            // The nodedAdded signal is called twice. When it is called the first time, 
+        if (self::hasReplicationConfiguration($node) && (self::nodeCreateReplicationEnabled($node) || self::nodeCreateHiddenEnabled($node)) && self::nodeHasAllowedPath($node)) {
+            // The nodedAdded signal is called twice. When it is called the first time,
             // the node is not created completely (the child nodes are not created yet), so we skip that call.
             if (count($node->getNodeType()->getAutoCreatedChildNodes()) > 0 && count($node->findChildNodes()) === 0) {
                 return;
@@ -31,14 +31,14 @@ class NodeSignalInterceptor
 
     public static function nodeRemoved(NodeInterface $node): void
     {
-        if (self::hasReplicationConfiguration($node) && self::nodeRemoveReplicationEnabled($node)) {
+        if (self::hasReplicationConfiguration($node) && self::nodeRemoveReplicationEnabled($node) && self::nodeHasAllowedPath($node)) {
             self::getNodeReplicator()->removeNodeVariants($node);
         }
     }
 
     public static function nodePropertyChanged(NodeInterface $node, string $propertyName, $oldValue, $newValue): void
     {
-        if (!self::hasReplicationConfiguration($node)) {
+        if (!self::hasReplicationConfiguration($node) || !self::nodeHasAllowedPath($node)) {
             return;
         }
 
@@ -70,6 +70,26 @@ class NodeSignalInterceptor
     protected static function nodeCreateHiddenEnabled(NodeInterface $node): bool
     {
         return $node->getNodeType()->hasConfiguration('options.replication.structure.createHidden') && $node->getNodeType()->getConfiguration('options.replication.structure.createHidden');
+    }
+
+    protected static function nodeHasAllowedPath(NodeInterface $node): bool
+    {
+        if (!$node->getNodeType()->hasConfiguration('options.replication.onlyPathsStartingWith')) {
+            return true;  // if not configured, return default: allowed = true
+        }
+
+        $allowedStartingPaths = $node->getNodeType()->getConfiguration('options.replication.onlyPathsStartingWith');
+        if (!is_array($allowedStartingPaths)) {  // fallback if only a string, instead of list was provided in the yaml
+            $allowedStartingPaths = [$allowedStartingPaths];
+        }
+        $nodePath = strval($node->findNodePath());
+
+        foreach ($allowedStartingPaths as $allowedPath) {
+            if (substr($nodePath, 0, strlen($allowedPath)) === $allowedPath) {  // str_starts_with for pre php 8
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
